718840ba78946746a96c840a8864f7abbac13589
Viktor Luft <viktor.luft@freiheit.com>
2022-11-15 10:20:14 +0100

    Fix coveralls upload

--- jest.config.mjs
+++ jest.config.mjs
@@ -2,7 +2,7 @@ export default {
   collectCoverageFrom: ['src/**/*.ts?(x)', '!src/index.ts'],
   testMatch: ["<rootDir>/test/**/(*.)test.ts?(x)"],
   coverageThreshold: { global: { statements: 100, branches: 100, functions: 100, lines: 100 } },
-  coverageDirectory: "<rootDir>/build/coverage",
+  coverageDirectory: "<rootDir>/coverage",
   testEnvironment: 'jsdom',
   transform: { "\\.(js|jsx|ts|tsx)$": "@sucrase/jest-plugin" },
 };
